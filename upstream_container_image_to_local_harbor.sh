391f52bc5f6743e08b0eea59af03d5fe20245f8b
Bogdan-Adrian Burciu <59748058+baburciu@users.noreply.github.com>
2022-05-27 18:28:45 +0300

    don't use image name fqdn in dest registry

--- upstream_container_image_to_local_harbor.sh
+++ upstream_container_image_to_local_harbor.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-# Fetch container image from upstream and push to local Harbor
-# Bogdan Adrian Burciu 23/05/2022 vers 1
-
+# Fetch container image from upstream and push to local registry
 # -------------------------
-# How to run: ./upstream_container_image_to_local_harbor.sh -s fluxcd/helm-controller:v0.21.0 -d <Harbor>/bogdan.burciu
+# How to run: 
+# export DOCKER_USERNAME=<destination_registry_username> DOCKER_PASSWORD=<destination_registry_pass>
+# ./upstream_container_image_to_local_harbor.sh -s fluxcd/helm-controller:v0.21.0 -d <Harbor>/bogdan.burciu
+#   or
+# ./upstream_container_image_to_local_harbor.sh -s quay.io/metal3-io/ip-address-manager:v1.1.2 -d <destination_registry_ip>:<destination_registry_port>
 
 
 # configure script to exit as soon as any line in the bash script fails and show that line
@@ -22,12 +24,22 @@ done
 echo -e "\t\t >> docker image pull $source_image"
 docker image pull $source_image
 source_image_repo=`echo $source_image | awk -F":" '{print $1}'`
+source_image_repo_after1stslash=${source_image_repo#*/}
+source_image_repo_before1stslash=${source_image_repo%"$source_image_repo_after1stslash"}
+
+# if the image name starts with the fqdn of a container registry, containing ".", then skip it from the name
+if [[ $source_image_repo_before1stslash == *"."* ]]; then
+    source_image_repo=$source_image_repo_after1stslash
+fi
+
 source_image_tag=`echo $source_image | awk -F":" '{print $2}'`
 image_id=`docker image ls | grep $source_image_repo | grep $source_image_tag | grep -v $destination_repo | awk {'print $3'}`
+echo $image_id
+echo $destination_repo
 echo -e "\t\t >> docker tag $image_id $destination_repo/$source_image_repo:$source_image_tag"
 docker tag $image_id $destination_repo/$source_image_repo:$source_image_tag
 destination_repo_fqdn=`echo $destination_repo | awk -F"/" '{print $1}'`
 echo -e "\t\t >> docker login $destination_repo_fqdn"
-docker login $destination_repo_fqdn
+docker login $destination_repo_fqdn -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 echo -e "\t\t >> docker push $destination_repo/$source_image_repo:$source_image_tag"
 docker push $destination_repo/$source_image_repo:$source_image_tag
